1f14bc433e07267af2acdc3665a6699d782be951
MohonSaha <practicewithmohon@gmail.com>
2022-05-18 11:35:54 +0600

    add new

--- thedogapi/js/index.js
+++ thedogapi/js/index.js
@@ -1,3 +1,20 @@
-fetch("https://api.thedogapi.com/v1/breeds")
-.then(Response => Response.json())
-.then(data => console.log(data))
+
+const loadDog = () => {
+    fetch("https://api.thedogapi.com/v1/breeds")
+    .then(Response => Response.json())
+    .then(data => displayDog(data))
+}
+
+const displayDog = (dogList) => {
+    const main = document.getElementById("main");
+    const first10Data = dogList.slice(0, 10);
+    
+    for(const dog of first10Data){
+        const div = document.createElement("div");
+        div.innerHTML= `
+            <h2>${dog.name} </h2>
+        `;
+    main.appendChild(div)
+    }
+
+}
