97b57d95d7fcc4141ef41a2628dbe690976b81b7
MohonSaha <practicewithmohon@gmail.com>
2022-05-20 14:10:26 +0600

    adding code

--- cardapi/js/index.js
+++ cardapi/js/index.js
@@ -1,11 +1,15 @@
 const searchButton = () => {
     const input = document.getElementById("input-value");
     const error = document.getElementById("error");
-    const inputValue = input.value;
+
+    const inputValue = parseInt(input.value);
     if(isNaN(inputValue) || inputValue == ""){
         // alert("please enter a number");
         error.innerText="please give a number";
         input.value = "";
+    }
+    else if(inputValue > 52){
+        error.innerText= "Please give a number Under 53"
     }
     else if(inputValue <= 0){
         error.innerText= "please give a positive number"
