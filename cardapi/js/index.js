e711bda4b689808676f0d11c04170281dde65195
MohonSaha <practicewithmohon@gmail.com>
2022-05-21 23:20:37 +0600

    add code

--- cardapi/js/index.js
+++ cardapi/js/index.js
@@ -1,3 +1,5 @@
+
+const main = document.getElementById("main");
 const searchButton = () => {
     const input = document.getElementById("input-value");
     const error = document.getElementById("error");
@@ -18,11 +20,27 @@ const searchButton = () => {
     else{
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
         .then(res => res.json())
-        .then(data => cardsDisplay(data))
+        .then(data => cardsDisplay(data.cards))
         input.value="";
     }
 }
 
 const cardsDisplay = (cards) => {
+    // cards = cards.cards;
     console.log(cards);
+    for (const card of cards){
+        console.log(card.image);
+        const div = document.createElement("div");
+        div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img src="${card.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+        `
+    main.appendChild(div);
+    }
 }
