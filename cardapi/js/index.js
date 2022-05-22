22b717a664b85106e902b352bfe7eed8eb9a0340
MohonSaha <practicewithmohon@gmail.com>
2022-05-22 17:13:37 +0600

    add code

--- cardapi/js/index.js
+++ cardapi/js/index.js
@@ -9,38 +9,51 @@ const searchButton = () => {
         // alert("please enter a number");
         error.innerText="please give a number";
         input.value = "";
+        main.innerHTML = "";
     }
     else if(inputValue > 52){
         error.innerText= "Please give a number Under 53"
+        main.innerHTML ="";
     }
     else if(inputValue <= 0){
         error.innerText= "please give a positive number"
         input.value="";
+        main.innerHTML ="";
     }
     else{
+        main.innerHTML="";
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
         .then(res => res.json())
         .then(data => cardsDisplay(data.cards))
         input.value="";
+        error.innerHTML ="";
     }
 }
 
 const cardsDisplay = (cards) => {
     // cards = cards.cards;
-    console.log(cards);
+    // console.log(cards);
     for (const card of cards){
         console.log(card.image);
         const div = document.createElement("div");
+        div.classList.add("col-lg-4");
+        div.classList.add("col-md-6");
+        div.classList.add("mb-4");
         div.innerHTML = `
                 <div class="card" style="width: 18rem;">
                     <img src="${card.image}" class="card-img-top" alt="...">
                     <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title">${card.suit}</h5>
+                    <p class="card-text">${card.code}</p>
+                    <button onclick="cardDetails('${card.code}')" href="#" class="btn btn-primary">See details</button>
                     </div>
                 </div>
         `
     main.appendChild(div);
     }
+}
+
+
+const cardDetails = (mohon) => {
+    console.log(mohon);
 }
